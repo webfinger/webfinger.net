0279f8447b23284fc87926abcf39262d8f319d21
Will Norris <will@willnorris.com>
2019-06-09 14:59:30 +0000

    update search input field immediately

--- js/lookup.js
+++ js/lookup.js
@@ -2,12 +2,13 @@ const params = new URLSearchParams(window.location.search);
 const resource = params.get('resource');
 
 if (resource) {
+  document.getElementById("resource").value = resource;
+
   fetch("https://client.webfinger.net/?resource="+resource)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      document.getElementById("resource").value = data.resource;
       document.getElementById("logs").innerText = data.logs;
       document.getElementById("jrd").innerText = JSON.stringify(data.jrd, null, 2);
     });
